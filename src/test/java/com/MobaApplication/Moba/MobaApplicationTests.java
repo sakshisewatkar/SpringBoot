7d7f7e2683fa640f7e7f1be7d345a20216bdf5b6
sakshisewatkar <136146512+sakshisewatkar@users.noreply.github.com>
2025-10-04 11:33:55 +0530

    first commit

---
+++ src/test/java/com/MobaApplication/Moba/MobaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MobaApplication.Moba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
